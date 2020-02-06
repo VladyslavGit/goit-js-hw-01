6097949067e710c7033e5e085991261ac6e5d7cb
Vladyslav <vladyslav.work@gmail.com>
2020-02-06 17:01:24 +0200

    04

--- hw-07/js/task-04.js
+++ hw-07/js/task-04.js
@@ -3,17 +3,14 @@ const counterValue = document.getElementById("value");
 const decrementBtn = document.querySelector('button[data-action="decrement"]');
 const incrementBtn = document.querySelector('button[data-action="increment"]');
 let clickCount = 0;
-const counterDecrement = function() {
+ function counterDecrement() {
   clickCount -= 1;
-  return (counterValue.textContent = clickCount);
+  counterValue.textContent = clickCount;
 };
-decrementBtn.addEventListener("click", (event) => {
-  counterDecrement(event);
-});
-const counterIncrement = function() {
+decrementBtn.addEventListener("click", counterDecrement);
+ function counterIncrement() {
   clickCount += 1;
-  return (counterValue.textContent = clickCount);
+  counterValue.textContent = clickCount;
 };
-incrementBtn.addEventListener("click", (event) => {
-  counterIncrement(event);
-});
+incrementBtn.addEventListener("click", 
+counterIncrement);
