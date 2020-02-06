82e897d4a2b9293ba4a4687e9517b167b71c48e9
Vladyslav <vladyslav.work@gmail.com>
2020-02-06 14:47:52 +0200

    04

--- hw-07/js/task-04.js
+++ hw-07/js/task-04.js
@@ -2,15 +2,17 @@
 const counterValue = document.getElementById("value");
 const decrementBtn = document.querySelector('button[data-action="decrement"]');
 const incrementBtn = document.querySelector('button[data-action="increment"]');
-
+let clickCount = 0;
 const counterDecrement = function() {
-  return (counterValue.textContent = parseInt(counterValue.textContent) - 1);
+  clickCount -=1;
+  return (counterValue.textContent = clickCount);
 };
 decrementBtn.addEventListener("click", () => {
   counterDecrement();
 });
 const counterIncrement = function() {
-  return (counterValue.textContent = parseInt(counterValue.textContent) + 1);
+  clickCount +=1;
+  return (counterValue.textContent =  clickCount);
 };
 incrementBtn.addEventListener("click", () => {
   counterIncrement();
--- hw-07/js/task-07.js
+++ hw-07/js/task-07.js
@@ -1,6 +1,6 @@
-"use strict";
-const input = document.getElementById("font-size-control");
-const span = document.getElementById("text");
+'use strict';
+const input = document.getElementById('font-size-control');
+const span = document.getElementById('text');
 input.addEventListener('input', handlerRange);
 function handlerRange(event) {
   span.style.fontSize = event.currentTarget.value + "px";
